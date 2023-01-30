145eb6610347090d27c9ce8f535740b506124c18
Abid Gaidarov <paantur@gmail.com>
2023-01-30 17:08:42 +0400

    fix

--- road_sign_bot.py
+++ road_sign_bot.py
@@ -22,8 +22,10 @@ from torchvision import transforms
 import torchvision.models as models
 from torchvision import transforms as T
 
+
 # Configure logging
 logging.basicConfig(level=logging.INFO)
+
 
 # Initialize bot and dispatcher
 API_TOKEN = config.TOKEN
@@ -33,8 +35,9 @@ dp = Dispatcher(bot)
 # Load first trained PyTorch model (YOLO)
 model1 = torch.hub.load('ultralytics/yolov5', 
                         'custom', 
-                        path='/Users/paantur/Documents/Учеба/Data Science/Elbrus Bootcamp/Final project/weights_model1.pt', 
+                        path='weights_model1.pt', 
                         force_reload=True)
+
 
 # Function to crop the photos and collect them in the list
 def crop_photo(photo_file):
@@ -49,17 +52,20 @@ def crop_photo(photo_file):
         list_of_crops.append(img)
     return list_of_crops
 
+
 # Load second trained PyTorch model (ResNet50)
 model2 = models.resnet50(pretrained=True)
 model2.fc = nn.Linear(in_features=2048, 
                       out_features=102)
-model2.load_state_dict(torch.load('/Users/paantur/Documents/Учеба/Data Science/Elbrus Bootcamp/Final project/weights_model2.pt', 
+model2.load_state_dict(torch.load('weights_model2.pt', 
                                   map_location=torch.device('cpu')))
 model2.eval()
 
+
 # Load the dataset with classes
-model2_df = pd.read_excel('/Users/paantur/Documents/Учеба/Data Science/Elbrus Bootcamp/Final project/classes_description.xlsx')
+model2_df = pd.read_excel('classes_description.xlsx')
 model2_df['class'] = model2_df['class'].astype('int')
+
 
 # Function to classify iamges
 def classify_photo(cropped_photo_path):
@@ -72,21 +78,25 @@ def classify_photo(cropped_photo_path):
     pred = torch.argmax(output, 1)
     return pred
 
+
 # Function to extract description
 def extract_descriptive(predicted_class):
     row = model2_df[model2_df['class']==predicted_class]
     return f"""CATEGORY: {row['category'].item()}, \nDESCRIPTION: {row['descriptive'].item()}"""
 
+
 # Start command handler
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    user_name = message.from_user.full_name
+    await message.reply(f"Hello, {user_name}! 🤖 I'm SignVision, your personal road sign detection and classification expert. 🦾 Powered by AI and computer vision technologies, I can help you identify the road signs in your photos with just a snap. Let's get started! 🚦")
 
 URI_INFO = f"https://api.telegram.org/bot{API_TOKEN}/getFile?file_id="
 URI = f"https://api.telegram.org/file/bot{API_TOKEN}/"
 
 if not os.path.exists('static'):
     os.mkdir('static')
+
 
 # Sent photo handler    
 @dp.message_handler(content_types=['photo'])
@@ -99,7 +109,7 @@ async def classify(msg: types.Message):
     model1_crop = crop_photo(img)
 
     if len(model1_crop) == 0:
-        await msg.answer('No sign has been found. Please try sending another photo.')
+        await msg.answer('I cannot classify it. 🧐 Please try sending another photo.')
     else:
         try:
             for n in model1_crop:
@@ -112,7 +122,13 @@ async def classify(msg: types.Message):
                 final_result = extract_descriptive(int(predicted_class))
                 await msg.answer(final_result)
         except:
-            await msg.answer('Error. Please try again.')
+            await msg.answer('I am not sure what sign it is. 🤗 Please try sending another photo.')
+    
+    url = 'https://memepedia.ru/wp-content/uploads/2020/09/b2b7c451cbddc634ecc0dc37031fb4d6.jpg'
+    response = requests.get(url)
+    with open('temp.jpg', 'wb') as f:
+        f.write(response.content)
+    await msg.answer_photo(photo=open('temp.jpg', 'rb'))
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False)

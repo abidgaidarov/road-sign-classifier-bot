14f86bf1711c6e1d14fa73afb9613a7544ee3dbe
Abid Gaidarov <paantur@gmail.com>
2023-01-31 12:38:41 +0400

    fix

--- road_sign_bot.py
+++ road_sign_bot.py
@@ -1,5 +1,7 @@
 #import libraries
 import aiogram
+import aiogram.utils
+import asyncio
 from aiogram.types import ContentType, Message
 from aiogram.dispatcher.filters import Command
 from aiogram import Bot, executor, Dispatcher, types
@@ -15,6 +17,7 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 import torch
 import torch.nn as nn
@@ -45,11 +48,12 @@ def crop_photo(photo_file):
     crops = result.crop(save=False)
     list_of_crops = []
     for n in range(len(crops)):
-        extracted_image = crops[n]['im']
-        converted_iamge = cv2.cvtColor(extracted_image, 
-                                       cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(converted_iamge)
-        list_of_crops.append(img)
+        if crops[n]['conf'].item() > 0.8: # confidence level for detection
+            extracted_image = crops[n]['im']
+            converted_iamge = cv2.cvtColor(extracted_image, 
+                                        cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(converted_iamge)
+            list_of_crops.append(img)
     return list_of_crops
 
 
@@ -57,7 +61,7 @@ def crop_photo(photo_file):
 model2 = models.resnet50(pretrained=True)
 model2.fc = nn.Linear(in_features=2048, 
                       out_features=102)
-model2.load_state_dict(torch.load('weights_model2.pt', 
+model2.load_state_dict(torch.load('weights2_new.pt', 
                                   map_location=torch.device('cpu')))
 model2.eval()
 
@@ -75,8 +79,9 @@ def classify_photo(cropped_photo_path):
     x = transform(img)
     x = x.unsqueeze(0) 
     output = model2(x.to('cpu')) 
-    pred = torch.argmax(output, 1)
-    return pred
+    pred = torch.argmax(output, 1).item()
+    pred_probability = torch.nn.functional.softmax(output, dim=1).max().item()
+    return pred, pred_probability
 
 
 # Function to extract description
@@ -84,12 +89,13 @@ def extract_descriptive(predicted_class):
     row = model2_df[model2_df['class']==predicted_class]
     return f"""CATEGORY: {row['category'].item()}, \nDESCRIPTION: {row['descriptive'].item()}"""
 
-
 # Start command handler
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    user_name = message.from_user.full_name
-    await message.reply(f"Hello, {user_name}! 🤖 I'm SignVision, your personal road sign detection and classification expert. 🦾 Powered by AI and computer vision technologies, I can help you identify the road signs in your photos with just a snap. Let's get started! 🚦")
+    full_name = message.from_user.full_name
+    user_name = message.from_user.username
+    await message.reply(f"Hello, {user_name}! 🤖 I'm SignVision, your personal road sign detection expert. 🦾 Powered by AI and computer vision technologies, I can help you identify the road signs in your photos with just a snap. Let's get started! 🚦")
+
 
 URI_INFO = f"https://api.telegram.org/bot{API_TOKEN}/getFile?file_id="
 URI = f"https://api.telegram.org/file/bot{API_TOKEN}/"
@@ -97,10 +103,22 @@ URI = f"https://api.telegram.org/file/bot{API_TOKEN}/"
 if not os.path.exists('static'):
     os.mkdir('static')
 
+async def delete_message(chat_id, message_id):
+    # Delete the message with the specified ID
+    await bot.delete_message(chat_id=chat_id, message_id=message_id)
 
 # Sent photo handler    
 @dp.message_handler(content_types=['photo'])
 async def classify(msg: types.Message):
+
+    msg_to_delete1 = await bot.send_message(chat_id=msg.chat.id, text='Thinking...🤔')
+
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    full_name = msg.from_user.full_name
+    user_name = msg.from_user.username
+    with open("usernames.txt", "a") as file:
+        file.write(f"{current_time}: {user_name} - {full_name} \n")
+
     file_id = msg.photo[-1].file_id
     resp = requests.get(URI_INFO + file_id)
     img_path = resp.json()['result']['file_path']
@@ -109,26 +127,37 @@ async def classify(msg: types.Message):
     model1_crop = crop_photo(img)
 
     if len(model1_crop) == 0:
-        await msg.answer('I cannot classify it. 🧐 Please try sending another photo.')
-    else:
-        try:
-            for n in model1_crop:
-                img_name = secrets.token_hex(8)
-                n.save(f'static/{img_name}.png', format='PNG')
-                await msg.answer_photo(photo=open(f'static/{img_name}.png', 'rb'))
+        await msg.answer('I cannot detect a sign. 🧐 Please try sending another photo.')
+    elif len(model1_crop) >= 1:
+        for n in model1_crop:
+            img_name = secrets.token_hex(8)
+            n.save(f'static/{img_name}.png', format='PNG')
+            photo=open(f'static/{img_name}.png', 'rb')
 
-                predicted_class = classify_photo(f'static/{img_name}.png').item()
-                os.remove(f'static/{img_name}.png')
-                final_result = extract_descriptive(int(predicted_class))
-                await msg.answer(final_result)
-        except:
-            await msg.answer('I am not sure what sign it is. 🤗 Please try sending another photo.')
+            predicted_class = classify_photo(f'static/{img_name}.png')[0]
+            predicted_class_probability = classify_photo(f'static/{img_name}.png')[1]
+            os.remove(f'static/{img_name}.png')
+            final_description = extract_descriptive(int(predicted_class))
+            
+            try:
+                await delete_message(chat_id=msg.chat.id, message_id=msg_to_delete1.message_id)
+            except:
+                None
+
+            print(predicted_class_probability)
+            if predicted_class_probability > 0.7: # confidence level for classification
+                await msg.answer_photo(photo)
+                await msg.answer(final_description)
+    else:
+        await msg.answer('I am not sure what sign it is. 🤗 Please try sending another photo.')
     
     url = 'https://memepedia.ru/wp-content/uploads/2020/09/b2b7c451cbddc634ecc0dc37031fb4d6.jpg'
     response = requests.get(url)
     with open('temp.jpg', 'wb') as f:
         f.write(response.content)
-    await msg.answer_photo(photo=open('temp.jpg', 'rb'))
+    msg_to_delete2 = await msg.answer_photo(photo=open('temp.jpg', 'rb'))
+    await asyncio.sleep(1.5)
+    await delete_message(chat_id=msg.chat.id, message_id=msg_to_delete2.message_id)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=False)
